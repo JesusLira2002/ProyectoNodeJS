d8e4842cecd7e4b24de6ff4db680e0a4b05725ad
JesusLira2002 <jlira28@alumnos.uaq.mx>
2025-03-22 19:11:04 -0600

    Actualizaciones Act 8

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,27 +2,38 @@ const express = require('express');
 const pokemon = express.Router();
 const db = require('../config/database');
 
-pokemon.post("/", (req, res, next) => {
-    return res.status(200).json(req.body);
+pokemon.post("/", async (req, res, next) => {
+    const { pok_namE, pok_height, pok_weight, pok_base_experience } = req.body;
+
+    if(pok_namE && pok_height && pok_weight && pok_base_experience) {
+        let query = "INSERT INTO pokemon (pok_namE, pok_height, pok_weight, pok_base_experience)";
+    query += ` VALUES('${pok_namE}', ${pok_height}, ${pok_weight}, ${pok_base_experience})`;
+
+    const rows = await db.query(query);
+
+    if (rows.affectedRows == 1) {
+        return res.status(201).json({ code: 201, message: "Pokemon insertado correctamente" });
+    }
+    return res.status(500).json({ code: 500, message: "Ocurrió un error" });
+}
+
+return res.status(500).json({ code: 500, message: "Campos incompletos" });
 });
 
 pokemon.get('/', async (req, res, next) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json({ code: 1, message: pkmn});
+    return res.status(200).json({ code: 200, message: pkmn});
 });
 
 pokemon.get('/:id([0-9]{1,3})', async(req, res, next) => {
     const id = req.params.id - 1;    
     if (id >= 1 && id <= 722) {
         const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id="+id+";");
-        return res.status(200).json({code :1, message:pkmn});
+        return res.status(200).json({code :200, message:pkmn});
         } 
         return res.status(404).json({
             code: 404,
             message: "Pokémon no encontrado",
-            hint1: "El ID del Pokémon tiene que ser desde 1 y hasta 722",
-            hint2: "Recuerda que es la primera generación de Pokémon",
-            hint3: "Verifica que el ID ingresado sea un número válido"
         });
         
 });
@@ -31,14 +42,11 @@ pokemon.get('/:name([A-Za-z]+)', async(req, res, next) => {
     const name = req.params.name;
     const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name='"+name+"';");{ 
     if (pkmn.length > 0) {
-        return res.status(200).json({code :1, message:pkmn});
+        return res.status(200).json({code :200, message:pkmn});
     } 
     return res.status(404).json({
         code: 404,
         message: "Pokémon no encontrado",
-        hint1: "Asegúrate de escribir correctamente el nombre del Pokémon",
-        hint2: "El nombre debe coincidir con el registrado en la base de datos",
-        hint3: "Recuerda que los nombres no son sensibles a mayúsculas o minúsculas"
     });
 }
 }
