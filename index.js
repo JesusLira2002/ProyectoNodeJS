c1f99229169326d5e94713798c473db784fbcdf5
JesusLira2002 <jlira28@alumnos.uaq.mx>
2025-05-17 21:45:32 -0600

    Cambios 17-05-2025 cors add

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ DELETE- Borrar un recurso
 const morgan = require('morgan');
 const  express = require('express');
 const app = express();
+
 //Routers
 const pokemon = require('./routes/pokemon');
 const user = require('./routes/user');
@@ -18,7 +19,10 @@ const user = require('./routes/user');
 const auth = require('./middleware/auth');
 const notFound = require('./middleware/notFound');
 const index = require ('./middleware/index');
+const cors = require('./middleware/cors');
 
+
+app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
